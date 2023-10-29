10033b29bcfa4599beaa05bf3c5f857467a52474
maltsevoff <alexmalczev@gmail.com>
2023-10-29 14:59:41 +0200

    add sign tabbar

---
+++ WalletConnectDemo/Modules/Sign/SignView.swift
@@ -0,0 +1,25 @@
+//
+//  SignView.swift
+//  WalletConnectDemo
+//
+//  Created by Aleksandr Maltsev on 29.10.2023.
+//
+
+import SwiftUI
+
+struct SignView: View {
+    let viewModel: SignViewModel
+
+    var body: some View {
+        VStack {
+            Button("Connect") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SignView(viewModel: SignViewModel())
+}
---
+++ WalletConnectDemo/Modules/Sign/SignViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignViewModel.swift
+//  WalletConnectDemo
+//
+//  Created by Aleksandr Maltsev on 29.10.2023.
+//
+
+import Foundation
+
+final class SignViewModel {
+    func connect() {
+        
+    }
+}
