576764132bd37a33b193b71f23f06cfc89561a25
maltsevoff <alexmalczev@gmail.com>
2023-10-29 12:01:41 +0200

    init project

---
+++ WalletConnectDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletConnectDemo
+//
+//  Created by Aleksandr Maltsev on 29.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletConnectDemo/WalletConnectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletConnectDemoApp.swift
+//  WalletConnectDemo
+//
+//  Created by Aleksandr Maltsev on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletConnectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
